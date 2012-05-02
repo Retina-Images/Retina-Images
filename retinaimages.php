bf85de250374638cd397ce4588ba505ea1f3b5eb
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-05-02 19:26:01 +1000

    Added debug logging.

--- retinaimages.php
+++ retinaimages.php
@@ -1,5 +1,8 @@
 <?php
-	/* Version: 1.0 */
+	
+	define('DEBUG', false);
+
+	/* Version: 1.1 */
 
 	$document_root  = $_SERVER['DOCUMENT_ROOT'];
 	$requested_uri  = parse_url(urldecode($_SERVER['REQUEST_URI']), PHP_URL_PATH);
@@ -7,17 +10,34 @@
 	$source_file    = $document_root.$requested_uri;
 	$source_ext     = strtolower(pathinfo($source_file, PATHINFO_EXTENSION));
 
+	if (DEBUG) {
+		$_debug_fh = fopen('retinaimages.log', 'a');
+		fwrite($_debug_fh, "* * * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n");
+		fwrite($_debug_fh, print_r($_COOKIE, true)."\n\n");
+		fwrite($_debug_fh, "document_root:     {$document_root}\n");
+		fwrite($_debug_fh, "requested_uri:     {$requested_uri}\n");
+		fwrite($_debug_fh, "requested_file:    {$requested_file}\n");
+		fwrite($_debug_fh, "source_file:       {$source_file}\n");
+		fwrite($_debug_fh, "source_ext:        {$source_ext}\n");
+	}
+
 	// Image was requested
 	if (in_array($source_ext, array('png', 'gif', 'jpg', 'jpeg', 'bmp'))) {
 
 		// Check if DPR is high enough to warrant retina image
+		if (DEBUG) { fwrite($_debug_fh, "devicePixelRatio:  {$_COOKIE['devicePixelRatio']}\n"); }
 		if (isset($_COOKIE['devicePixelRatio']) && intval($_COOKIE['devicePixelRatio']) > 1) {
 			// Check if retina image exists
 			$retina_file = pathinfo($source_file, PATHINFO_DIRNAME).'/'.pathinfo($source_file, PATHINFO_FILENAME).'@2x.'.pathinfo($source_file, PATHINFO_EXTENSION);
+			if (DEBUG) { fwrite($_debug_fh, "retina_file:       {$retina_file}\n"); }
 			if (file_exists($retina_file)) {
 				$source_file = $retina_file;
 			}
 		}
+
+		// Close debug session if open
+		if (DEBUG) { fwrite($_debug_fh, "sending file:      {$source_file}\n\n"); }
+		if (DEBUG) { fclose($_debug_fh); }
 
 		// Send headers
 		if (in_array($source_ext, array('png', 'gif', 'jpeg', 'bmp'))) {
