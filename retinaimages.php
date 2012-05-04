d18a7d03ccf21caf60d7a46431bdf5c8e577fe25
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-05-04 23:25:52 +1000

    Added sending of expires headers.

--- retinaimages.php
+++ retinaimages.php
@@ -1,8 +1,10 @@
 <?php
-	
-	define('DEBUG', false);
 
 	/* Version: 1.2 */
+	
+	define('DEBUG',        false);       // Write debugging information to a log file
+	define('SEND_EXPIRES', true);        // 
+	define('EXPIRES_TIME', 24*60*60); // default: 1 day
 
 	$document_root  = $_SERVER['DOCUMENT_ROOT'];
 	$requested_uri  = parse_url(urldecode($_SERVER['REQUEST_URI']), PHP_URL_PATH);
@@ -44,10 +46,15 @@
 		// Send headers
 		if (in_array($source_ext, array('png', 'gif', 'jpeg', 'bmp'))) {
 			header("Content-Type: image/".$source_ext);
-		} else {
+		}
+		else {
 			header("Content-Type: image/jpeg");
 		}
 		header('Content-Length: '.filesize($source_file));
+		if (SEND_EXPIRES) {
+			date_default_timezone_set('GMT');
+			header('Expires: '.gmdate('D, m M Y G:i:s', time()+EXPIRES_TIME).' GMT');
+		}
 
 		// Send file
 		readfile($source_file);
