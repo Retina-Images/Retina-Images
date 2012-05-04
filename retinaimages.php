3ce8306b7a06d240cbeb5575e66d1aeee80e4868
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-05-05 01:08:26 +1000

    Added sending and checking of ETags and changed up some of the other headers. This and the previous bunch of commits should close #5

--- retinaimages.php
+++ retinaimages.php
@@ -1,9 +1,9 @@
 <?php
 
-	/* Version: 1.2 */
+	/* Version: 1.3 - now with more cache */
 	
 	define('DEBUG',              false);    // Write debugging information to a log file
-	define('SEND_PRAGMA',        true);     // 
+	define('SEND_ETAG',          true);     // You will want to disable this if you load balance multiple servers 
 	define('SEND_EXPIRES',       true);     // 
 	define('SEND_CACHE_CONTROL', true);     // 
 	define('CACHE_TIME',         24*60*60); // default: 1 day
@@ -39,24 +39,33 @@
 			}
 		}
 
-		// Close debug session if open
-		if (DEBUG) {
-			fwrite($_debug_fh, "sending file:      {$source_file}\n\n");
-			fclose($_debug_fh);
-		}
 
 		// Send cache headers
-		if (SEND_PRAGMA) {
-			header('Pragma: private');  
-		}
 		if (SEND_CACHE_CONTROL) {
-			header('Cache-Control: private, max-age='.CACHE_TIME.', pre-check='.CACHE_TIME);  
+			header('Cache-Control: private, max-age='.CACHE_TIME, true);  
 		}
 		if (SEND_EXPIRES) {
 			date_default_timezone_set('GMT');
-			header('Expires: '.gmdate('D, d M Y H:i:s', time()+CACHE_TIME).' GMT');
+			header('Expires: '.gmdate('D, d M Y H:i:s', time()+CACHE_TIME).' GMT', true);
 		}
-		if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && (strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == filemtime($source_file))) {
+		if (SEND_ETAG) {
+			$etag = '"'.fileinode($source_file).'"';
+			header("ETag: $etag", true);
+
+			if (DEBUG) {
+				fwrite($_debug_fh, "generated etag:    {$etag}\n");
+				if (isset($_SERVER['HTTP_IF_NONE_MATCH'])) {
+					fwrite($_debug_fh, "received etag:     {$_SERVER['HTTP_IF_NONE_MATCH']}\n\n");
+				}
+			}
+
+			if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && ($_SERVER['HTTP_IF_NONE_MATCH']) === $etag) {
+				// File in cache hasn't change
+				header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($source_file)).' GMT', true, 304);
+				exit();
+			}
+		}
+		if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && (strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) === filemtime($source_file))) {
 			// File in cache hasn't change
 			header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($source_file)).' GMT', true, 304);
 			exit();
@@ -64,12 +73,18 @@
 
 		// Send image headers
 		if (in_array($source_ext, array('png', 'gif', 'jpeg', 'bmp'))) {
-			header("Content-Type: image/".$source_ext);
+			header("Content-Type: image/".$source_ext, true);
 		}
 		else {
-			header("Content-Type: image/jpeg");
+			header("Content-Type: image/jpeg", true);
 		}
-		header('Content-Length: '.filesize($source_file));
+		header('Content-Length: '.filesize($source_file), true);
+
+		// Close debug session if open
+		if (DEBUG) {
+			fwrite($_debug_fh, "sending file:      {$source_file}\n\n");
+			fclose($_debug_fh);
+		}
 
 		// Send file
 		readfile($source_file);
@@ -79,9 +94,6 @@
 	// DPR value was sent
 	elseif(isset($_GET['devicePixelRatio'])) {
 		$dpr = $_GET['devicePixelRatio'];
-
-		// Respond with a success content
-		header('HTTP/1.1 200 OK');
 
 		// Validate value before setting cookie
 		if (''.intval($dpr) !== $dpr) {
@@ -93,4 +105,4 @@
 	}
 
 	// Respond with an empty content
-	header('HTTP/1.1 204 No Content');
+	header('HTTP/1.1 204 No Content', true);
