e2dffc31144769d99d8bd45287fb52cec4be5341
Jeremy Worboys <jw@jeremyworboys.com>
2013-07-26 09:52:08 +1000

    Clean up

--- retinaimages.php
+++ retinaimages.php
@@ -4,8 +4,8 @@
 
     define('DEBUG',              false);    // Write debugging information to a log file
     define('SEND_ETAG',          true);     // You will want to disable this if you load balance multiple servers
-    define('SEND_EXPIRES',       true);     //
-    define('SEND_CACHE_CONTROL', true);     //
+    define('SEND_EXPIRES',       true);
+    define('SEND_CACHE_CONTROL', true);
     define('DOWNSIZE_NOT_FOUND', true);     // If a regular image is requested and not found, send a retina file instead?
     define('CACHE_TIME',         24*60*60); // default: 1 day
     define('DISABLE_RI_HEADER',  false);
