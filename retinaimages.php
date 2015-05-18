862bff7455f07ad8cf1a225d8d4a4463a873a190
Jeremy Worboys <jw@jeremyworboys.com>
2015-05-18 19:12:56 +1000

    Don't convert filename case
    Fixes #37

--- retinaimages.php
+++ retinaimages.php
@@ -15,9 +15,9 @@
     $requested_uri   = parse_url(urldecode($_SERVER['REQUEST_URI']), PHP_URL_PATH);
     $requested_file  = basename($requested_uri);
     $source_file     = $document_root.$requested_uri;
-    $source_dirname  = strtolower(pathinfo($source_file, PATHINFO_DIRNAME));
-    $source_filename = strtolower(pathinfo($source_file, PATHINFO_FILENAME));
-    $source_ext      = strtolower(pathinfo($source_file, PATHINFO_EXTENSION));
+    $source_dirname  = pathinfo($source_file, PATHINFO_DIRNAME);
+    $source_filename = pathinfo($source_file, PATHINFO_FILENAME);
+    $source_ext      = pathinfo($source_file, PATHINFO_EXTENSION);
     $at2x_file       = $source_dirname.'/'.$source_filename.'@2x.'.$source_ext;
     $at3x_file       = $source_dirname.'/'.$source_filename.'@3x.'.$source_ext;
     $at4x_file       = $source_dirname.'/'.$source_filename.'@4x.'.$source_ext;
